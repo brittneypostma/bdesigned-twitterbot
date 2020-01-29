e38254e1de759463b81faf5c30d79004d1188c22
Brittney <brittney.postma@hotmail.com>
2020-01-29 15:50:16 -0500

    idk

--- bdesigned.py
+++ bdesigned.py
@@ -1,4 +1,3 @@
-
 from os import environ
 import time
 import tweepy
@@ -8,18 +7,44 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_TOKEN = environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
 
+# # # # TWITTER AUTHENTICATOR # # # #
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user = api.me()
-print(user.name)
+
+# # # # TWITTER STREAMER # # # #
+
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+        else:
+            print status_code
+
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+search = ['Python', 'JavaScript', 'WebDev', 'WomenWhoCode', 'MomsCanCode', 'zerotomastery', 'ztm',
+          'Zero To Mastery', 'Programmer', 'svelte', 'sveltejs', 'sapper', 'BrittneyPostma', 'b.Designed', 'bDesigned']
+
+myStream.filter(track=search, languages=["en"])
+
 
 # public_tweets = api.home_timeline()
 # for tweet in public_tweets:
 #     print(tweet.text.encode("utf-8"))
 
+# api.update_status('b.Designed Bot is now working! #python #tweepy')
 
+# # # # Limit Handler # # # #
 def limit_handler(cursor):
     while True:
         try:
@@ -28,24 +53,23 @@ def limit_handler(cursor):
             time.sleep(15 * 60)
 
 
-# api.update_status('b.Designed Bot is now working! #python #tweepy')
-
-
-# Narcissist bot
-search = '#zerotomastery OR #ztm OR #svelte OR @svelte OR #javascript OR #webdev #womenwhocode OR #momscancode OR @WomenWhoCode'
-totalItems = 10
-
-for tweet in tweepy.Cursor(api.search, search).items(totalItems):
-    try:
-        tweet.favorite()  # likes
-        tweet.retweet()  # retweets
-        print('It worked')
-    except tweepy.TweepError as err:
-        print(err.reason)
-    except StopIteration:
-        break
-
-# generous bot
+# # # # generous bot # # # #
 for follower in limit_handler(tweepy.Cursor(api.followers).items()):
-    follower.follow()
-    break
+    if not follower.following:
+        follower.follow()
+        print("Now following ", follower.name)
+
+
+# # # # Narcissist bot # # # #
+# search = '#zerotomastery OR #ztm OR #svelte OR @svelte OR #javascript OR #webdev #womenwhocode OR #momscancode OR @WomenWhoCode OR #python OR #programmer @andreineogoie'
+# totalItems = 10
+
+# for tweet in tweepy.Cursor(api.search, search).items(totalItems):
+#     try:
+#         tweet.favorite()  # likes
+#         tweet.retweet()  # retweets
+#         print('It worked')
+#     except tweepy.TweepError as err:
+#         print(err.reason)
+#     except StopIteration:
+#         break
