5d3739e8ca713e3d86cfa6d019bb6190ad7b8ef5
sballgirl11 <brittney.postma@hotmail.com>
2020-02-09 10:20:44 -0500

    print

--- search.py
+++ search.py
@@ -8,6 +8,7 @@ def check_mentions(api, keywords, since_id):
     for tweet in tweepy.Cursor(api.mentions_timeline,
                                since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
+        print(tweet.id, new_since_id)
         if tweet.in_reply_to_status_id is not None:
             continue
         if any(keyword in tweet.text.lower() for keyword in keywords):
